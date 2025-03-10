93565109363e515aad21c84a5c233656e41b524b
Nathaniel Henry <nat.henry7@gmail.com>
2025-03-10 16:10:06 -0700

    Document miscellaneous helper functions.

--- R/misc.R
+++ R/misc.R
@@ -1,9 +1,17 @@
 ## Helper functions
 
 #' Stop without listing the containing function call
+#'
+#' @param ... Parameters passed to `stop()`
+#'
+#' @return Concisely stops program execution
 qstop <- function(...) stop(..., call. = FALSE)
 
 #' Require that a namespace be loaded, or stop execution
+#'
+#' @param pkg (`character(1)`) Package to be loaded
+#'
+#' @return Silently loads namespace, or stops execution if package cannot be loaded
 require_namespace_or_stop <- function(pkg) {
   if (!requireNamespace(pkg, quietly = TRUE)){
     paste('Package', pkg, 'is required.') |> qstop()
